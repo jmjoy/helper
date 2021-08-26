6e452fc3ae7256c0b02615e2fe281079f9c749ff
jmjoy <918734043@qq.com>
2021-08-26 13:50:26 +0800

    Add some unwrap_or serial macros.

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,59 @@
+#![warn(rust_2018_idioms, non_ascii_idents)]
+#![warn(clippy::dbg_macro, clippy::print_stdout)]
+#![doc = include_str!("../README.md")]
+
 /// This is a useful macro to write expression like `?:` or `if expr { left } else { right }`.
 ///
 /// # Example
 ///
 /// ```
-/// use helper::r#if;
+/// use helper::if_or_else;
 ///
 /// let s = "hello";
 /// let len = s.len();
-/// let len = r#if!(len > 10, 10, len);
+/// let len = if_or_else!(len > 10, 10, len);
 /// assert_eq!(len, 5);
 /// ```
 #[macro_export]
-macro_rules! r#if {
+macro_rules! if_or_else {
     ($cond:expr, $left:expr, $right:expr) => {
         if $cond { $left } else { $right }
+    };
+}
+
+#[macro_export]
+macro_rules! unwrap_or_return {
+    ($x:expr) => {
+        unwrap_or_return!($x)
+    };
+    ($x:expr, $ret:expr) => {
+        match x {
+            Some(x) => x,
+            None => return $ret,
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! unwrap_or_break {
+    ($x:expr) => {
+        unwrap_or_break!($x)
+    };
+    ($x:expr, $ret:expr) => {
+        match x {
+            Some(x) => x,
+            None => break $ret,
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! unwrap_or_continue {
+    ($x:expr) => {
+        match x {
+            Some(x) => x,
+            None => continue,
+        }
     };
 }
 
@@ -22,9 +62,9 @@ mod test {
     use super::*;
 
     #[test]
-    fn test_if() {
+    fn test_if_or_else() {
         let a = 1;
-        assert_eq!(r#if!(a > 0, 1, 2), 1);
-        assert_eq!(r#if!(a > 1, 1, 2), 2);
+        assert_eq!(if_or_else!(a > 0, 1, 2), 1);
+        assert_eq!(if_or_else!(a > 1, 1, 2), 2);
     }
 }
